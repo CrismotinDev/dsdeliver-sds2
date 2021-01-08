b5b5fb3fce7c6e1feb449c432292231f278d7c1b
Cristiane Motin <crismotin2019@gmail.com>
2021-01-08 18:15:21 -0300

    Add Footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,11 +1,27 @@
 import './styles.css';
-import {ReactComponent as Logo} from './logo.svg';
+import {ReactComponent as YouTubeIcon} from './yotube.svg';
+import {ReactComponent as LinKedinIcon} from './linkedin.svg';
+import {ReactComponent as IstagramIcon} from './instagram.svg';
 
 function Footer(){
     return(
         <footer className="main=footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.yotube.com/c/DevSuperior" target="_new">
+                <YouTubeIcon />
 
+                </a>
+
+                <a href="https://linkedin.com/school/devsuperior/" target="_new">
+                <LinKedinIcon />
+                </a>
+
+                <a href="https://instagram.com/devsuperior.ig/">
+                <IstagramIcon />
+                </a>
+
+            </div>
         </footer>
     )
 }
